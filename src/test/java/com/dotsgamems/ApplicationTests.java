5c8e25da97c6359a369e2adc41f542d471625074
Marat Mingazov <marat_mingazov@epam.com>
2022-11-07 14:59:24 +0300

    Class rename

--- src/test/java/com/dotsgamems/ApplicationTests.java
+++ src/test/java/com/dotsgamems/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
 
 @SpringBootTest(properties = {InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false", ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"})
-class DotsGameMsApplicationTests {
+class ApplicationTests {
 
 
 	@Test
