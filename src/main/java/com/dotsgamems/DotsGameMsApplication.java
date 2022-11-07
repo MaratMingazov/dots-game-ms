cb432c3f05455dcf7f7c3272d54a3a518bb29849
Marat Mingazov <maratmingazovr@gmail.com>
2022-11-07 14:50:56 +0300

    Initial commit

---
+++ src/main/java/com/dotsgamems/DotsGameMsApplication.java
@@ -0,0 +1,13 @@
+package com.dotsgamems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotsGameMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotsGameMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotsgamems/DotsGameMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dotsgamems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+
+@SpringBootTest(properties = {InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false", ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"})
+class DotsGameMsApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
